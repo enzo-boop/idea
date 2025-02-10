c89e068fa9f09d46b9a2d9128a2264519346cb35
Vincenzo <vincenzodnm@outlook.it>
2025-02-10 14:35:33 +0100

    [FIX] update useQuery package for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { Post } from "./globals/models/models";
 import { getPosts } from "@/client-services/post-service";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Array<Post> | null>(null);
-
-  useEffect(() => {
-    getPosts().then((posts) => setPosts(posts));
-  }, []);
+  // const { data, error, isLoading } = useQuery(["data"], async () => {
+  //   const response = await getPosts();
+  //   if (!response.ok) throw new Error("Failed to fetch");
+  //   return response;
+  // });
 
   return (
     <div className="prose dark:prose-invert">
-      {posts?.map((res, index) => (
+      {/* {posts?.map((res, index) => (
         <div key={index} className="mt-4">
           <div className="post">
             <h3>{res.author}</h3>
@@ -31,7 +31,7 @@ export default function Home() {
             </span>
           </div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
